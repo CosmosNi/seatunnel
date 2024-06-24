47ded4f86c82af717aba7cb1af216e11a8931e44
CosmosNi <627165587@qq.com>
2024-06-24 11:01:45 +0800

    [Feature][Elasticsearch] Support multi-table sink write

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-elasticsearch-e2e/src/test/java/org/apache/seatunnel/e2e/connector/elasticsearch/ElasticsearchIT.java
@@ -17,7 +17,6 @@
 
 package org.apache.seatunnel.e2e.connector.elasticsearch;
 
-import com.google.common.collect.Maps;
 import org.apache.seatunnel.shade.com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.seatunnel.shade.com.fasterxml.jackson.databind.ObjectMapper;
@@ -51,6 +50,7 @@ import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.DockerLoggerFactory;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -186,13 +186,16 @@ public class ElasticsearchIT extends TestSuiteBase implements TestResource {
     public void testElasticsearchWithMultiSink(TestContainer container)
             throws IOException, InterruptedException {
         Container.ExecResult execResult =
-                container.executeJob("/elasticsearch/fakeSource_source_to_elasticsearch_multi_sink.conf");
+                container.executeJob(
+                        "/elasticsearch/fakeSource_source_to_elasticsearch_multi_sink.conf");
         Assertions.assertEquals(0, execResult.getExitCode());
         List<String> sinkIndexData5 = readMultiSinkData5("st_index5");
         List<String> sinkIndexData6 = readMultiSinkData6("st_index6");
-        String stIndex5 = "{\"id\":1,\"val_bool\":\"true\",\"val_tinyint\":1,\"val_smallint\":2,\"val_int\":3," +
-                "\"val_bigint\":4,\"val_float\":4.3,\"val_double\":5.3,\"val_decimal\":6.3,\"val_string\":\"NEW\"}";
-        String stIndex6 = "{\"id\":1,\"val_bool\":\"true\",\"val_tinyint\":1,\"val_smallint\":2,\"val_int\":3,\"val_bigint\":4,\"val_float\":4.3,\"val_double\":5.3,\"val_decimal\":6.3}";
+        String stIndex5 =
+                "{\"id\":1,\"val_bool\":\"true\",\"val_tinyint\":1,\"val_smallint\":2,\"val_int\":3,"
+                        + "\"val_bigint\":4,\"val_float\":4.3,\"val_double\":5.3,\"val_decimal\":6.3,\"val_string\":\"NEW\"}";
+        String stIndex6 =
+                "{\"id\":1,\"val_bool\":\"true\",\"val_tinyint\":1,\"val_smallint\":2,\"val_int\":3,\"val_bigint\":4,\"val_float\":4.3,\"val_double\":5.3,\"val_decimal\":6.3}";
         Assertions.assertIterableEquals(Lists.newArrayList(stIndex5), sinkIndexData5);
         Assertions.assertIterableEquals(Lists.newArrayList(stIndex6), sinkIndexData6);
     }
@@ -299,6 +302,7 @@ public class ElasticsearchIT extends TestSuiteBase implements TestResource {
                         "c_timestamp");
         return getDocsWithTransformTimestamp(source, index);
     }
+
     private List<String> readMultiSinkData5(String index) throws InterruptedException {
         // wait for index refresh
         Thread.sleep(2000);
@@ -314,7 +318,8 @@ public class ElasticsearchIT extends TestSuiteBase implements TestResource {
                         "c_double",
                         "c_decimal",
                         "c_string");
-        ScrollResult scrollResult = esRestClient.searchByScroll(index, source, Maps.newHashMap(), "1m", 1000);
+        ScrollResult scrollResult =
+                esRestClient.searchByScroll(index, source, Maps.newHashMap(), "1m", 1000);
         scrollResult
                 .getDocs()
                 .forEach(
@@ -347,7 +352,8 @@ public class ElasticsearchIT extends TestSuiteBase implements TestResource {
                         "c_float",
                         "c_double",
                         "c_decimal");
-        ScrollResult scrollResult = esRestClient.searchByScroll(index, source, Maps.newHashMap(), "1m", 1000);
+        ScrollResult scrollResult =
+                esRestClient.searchByScroll(index, source, Maps.newHashMap(), "1m", 1000);
         scrollResult
                 .getDocs()
                 .forEach(
